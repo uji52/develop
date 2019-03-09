e0be08a584a2be128ddb413af7098ec19662bafc
uji52 <uji.52724@gmail.com>
2019-03-09 08:52:10 +0000

    git

---
+++ git/init.sh
@@ -0,0 +1,15 @@
+git config --global user.name uji52
+git config --global user.email 'uji.52724@gmail.com'
+git config --global push.default current
+git config --global core.autocrlf false
+git config --global core.ignorecase false
+git config --global color.ui true
+git config --global alias.st status
+git config --global alias.df diff
+git config --global alias.co commit
+git config --global alias.br branch
+git config --global alias.ps push
+git config --global alias.pl pull
+git config --global alias.ck checkout
+git config --global core.quotepath false
+git config --global credential.helper 'cache --timeout=86400'
