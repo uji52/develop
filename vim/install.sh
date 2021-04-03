37c0e8a74918d347b101268b702717e864500962
uji52 <uji.52724@gmail.com>
2021-04-03 11:25:17 +0900

    vimrc設定shell追加

---
+++ vim/install.sh
@@ -0,0 +1,8 @@
+cp .vimrc ~/
+mkdir -p /home/uji52/.cache/dein/repos/github.com/Shougo/
+cd /home/uji52/.cache/dein/repos/github.com/Shougo/
+git clone https://github.com/Shougo/dein.vim
+cd dein.vim
+git fetch --tags
+git checkout 1.5
+cd 
